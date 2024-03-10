bde72685a394dd4cc9ee5b2150a68dc9855b0e4c
Mauro Candela <mauro.daniel.candela@gmail.com>
2024-03-10 20:31:23 -0300

    Tarea Terminada

---
+++ index.js
@@ -0,0 +1,106 @@
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+  },
+
+  {
+    id: 3,
+    nombre: "pizza Napolitana",
+    precio: 1350,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas"],
+  },
+
+  {
+    id: 4,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+  },
+
+  {
+    id: 5,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas", "Cebolla"],
+  },
+
+  {
+    id: 6,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+  },
+];
+
+
+// a)  Las pizzas que tengan un id impar.
+
+function mostrarImpares(pizzas) {
+
+  const pizzasConIdImpar = pizzas.filter(function(pizza) {
+    return pizza.id % 2 !== 0;
+  });
+
+  pizzasConIdImpar.forEach(function(pizza) {
+    console.log("La pizza con el id impar:", pizza.id, " es la",pizza.nombre);
+  });
+}
+
+mostrarImpares(pizzas);
+
+
+// b) Responder: ¿Hay alguna pizza que valga menos de $600?
+
+function respuestaPrecio(pizzas){
+
+  const pizzasMenoresA = pizzas.some(function(pizza){
+    return pizza.precio < 600; 
+  });
+
+  if (pizzasMenoresA== true){
+    console.log("Hay pizzas que valen menos de $600")
+  } else{
+    console.log("No hay pizzas que valgan menos de $600")
+  }
+ };
+
+respuestaPrecio(pizzas);
+
+// c) El nombre de cada pizza con su respectivo precio.
+function respuestaPizza(pizzas){
+pizzas.forEach( pizzas =>{
+  console.log("La ",pizzas.nombre,"tiene un valor de: $",pizzas.precio);
+});
+}
+
+respuestaPizza(pizzas);
+// d) Todos los ingredientes de cada pizza (En cada iteración imprimir los ingredientes de la pizza que se esta recorriendo). 
+
+function mostrarIngredientes(pizzas) {
+  
+  pizzas.forEach(function(pizza) {
+    console.log("Los ingredientes de la", pizza.nombre, "son :");
+
+    pizza.ingredientes.forEach(function(ingrediente) {
+      console.log(ingrediente);
+    });
+  });
+}
+
+mostrarIngredientes(pizzas);
